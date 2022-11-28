f745a8f69b88348be8be94e633cc11725f6ee272
unknown <arabkhanramazanov@gmail.com>
2022-11-28 16:13:43 +0400

    CountryDaoIml added

--- src/main/java/com/company/dao/impl/CountryDaoImpl.java
+++ src/main/java/com/company/dao/impl/CountryDaoImpl.java
@@ -8,44 +8,131 @@ import com.company.entity.Country;
 import com.mycompany.dao.inter.AbstractDAO;
 import com.mycompany.dao.inter.CountryDaoInter;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  *
- * @author SMART
+ * @author TURAL
  */
-public class CountryDaoImpl extends AbstractDAO implements CountryDaoInter{
+public class CountryDaoImpl extends AbstractDAO implements CountryDaoInter {
 
-    private Country getCountry(ResultSet rs) throws Exception {
-        int id = rs.getInt("id");
+    public Country getCountry(ResultSet rs) throws SQLException {
+
+        int id = rs.getInt("Id");
         String name = rs.getString("name");
         String nationality = rs.getString("nationality");
-        
-        return new Country(id, name, nationality);
-        
+
+        Country contry = new Country(id, name, nationality);
+        System.out.println(contry);
+        return contry;
+
     }
-    
-    
+
     @Override
     public List<Country> getAll() {
-        List<Country> result = new ArrayList<>();
-        try ( Connection c = connect()) {
-            Statement stmt = c.createStatement();
-            stmt.execute("select * from country");
+        List<Country> list = new ArrayList<>();
+        Connection conn;
+        try {
+            conn = connect();
+
+            Statement stmt = conn.createStatement();
+            stmt.execute("SELECT * FROM country");
             ResultSet rs = stmt.getResultSet();
 
             while (rs.next()) {
-                Country u = getCountry(rs);
 
-                result.add(u);
+                Country contry = getCountry(rs);
+                list.add(contry);
+
             }
+        } catch (Exception ex) {
+
+        }
+        return list;
+    }
+
+    @Override
+    public Country getById(int userId) {
+        Country el = null;
+        Connection conn;
+        try {
+            conn = connect();
+
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM country WHERE ID = ?");
+            stmt.setInt(1, userId);
+            stmt.execute();
+
+            ResultSet rs = stmt.getResultSet();
+
+            while (rs.next()) {
+
+                el = getCountry(rs);
+
+            }
+        } catch (Exception ex) {
+
+        }
+        return el;
+    }
+
+    @Override
+    public boolean updateCountry(Country u) {
+        Connection conn;
+        boolean b = true;
+        try {
+            conn = connect();
+            PreparedStatement stmt = conn.prepareStatement("UPDATE country SET name=?,nationality=? WHERE id= ?");
+            stmt.setString(1, u.getName());
+            stmt.setString(2, u.getNationality());
+            stmt.setInt(3, u.getId());
+            b = stmt.execute();
 
         } catch (Exception ex) {
-            ex.printStackTrace();
+            System.err.println(ex);
+            b = false;
         }
-        return result;
+        return b;
     }
+
+    @Override
+    public boolean insertCountry(Country u) {
+        Connection conn;
+        boolean b = true;
+        try {
+            conn = connect();
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO country (name ,nationality) VALUES (?,?)",Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, u.getName());
+            stmt.setString(2, u.getNationality());
+            b = stmt.execute();
+            ResultSet genKeys = stmt.getGeneratedKeys();
+            if(genKeys.next()){
+                u.setId(genKeys.getInt(1));
+            }
+        } catch (Exception ex) {
+            System.err.println(ex);
+            b = false;
+        }
+        return b;
+    }
+
+    @Override
+    public boolean removeCountry(int id) {
+        Connection conn;
+        try {
+            conn = connect();
+
+            PreparedStatement stmt = conn.prepareStatement("DELETE FROM country WHERE id=?;");
+            stmt.setInt(1, id);
+
+            return stmt.execute();
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
 }
